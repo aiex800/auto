a66f165e3b1b9c6ce619f2f761fb4ebd8e7415bc
Tor Eriksson <tor.eriksson@learnet.se>
2019-12-03 09:31:34 +0100

    Fungerande CSV läsning, lösenordsgenerator och CMD kommand

--- auto.py
+++ auto.py
@@ -1,12 +1,38 @@
 import subprocess, platform, csv, random, string
 
 filnamn = input("Ange .csv filnamn: ")
-
-
-with open(str(filnamn), ".csv", newline='') as myFile:
+lista = []
+listRow = 1
+with open(str(filnamn), newline='') as myFile:
     reader = csv.reader(myFile)
     for row in reader:
         lista.append(row)
 
 def listAssign():
-    
+    uName = str(lista[1][0])
+    uGivenName = str(lista[1][1])
+    uSurname = str(lista[1][2])
+    uAccountName = str(lista[1][3])
+    return (uName,uGivenName,uSurname,uAccountName)
+
+def createPassword():
+    chars = "abcdefghijklmnopqrstuvwxyz"
+    chars1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    chars2 = "1234567890"
+    chars3 = "!%&/()=#"
+    uPassword = ""
+    for c in range (3):
+        uPassword += random.choice(chars)
+        uPassword += random.choice(chars1)
+        uPassword += random.choice(chars2)
+        uPassword += random.choice(chars3)    
+
+    uPassword = ''.join(random.sample(uPassword,len(uPassword)))
+    return uPassword
+
+uPassword = createPassword()
+uName, uGivenName, uSurname, uAccountName = listAssign()
+listAssign()
+createPassword()
+cmd = 'New-ADUser -name "' + uName + '" -GivenName "' + uGivenName + '" -Surname "' + uSurname + '" -SamAccountName "' + uAccountName + '" -AccountPassword "' + uPassword + '" -Enable $true'
+print(cmd)
