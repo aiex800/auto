eee97e21b61482faba49f6be3c105b8c5c108190
Tor Eriksson <tor.eriksson@learnet.se>
2019-12-03 10:39:04 +0100

    If sats fungerar, ändra variabler i loop strular

--- auto.py
+++ auto.py
@@ -1,18 +1,30 @@
-import subprocess, platform, csv, random, string
+import subprocess, platform, csv, random, string, time
 
-filnamn = input("Ange .csv filnamn: ")
+try:
+    filnamn = str(input("Ange .csv filnamn: "))
+except:
+    print("Error")
+    time.sleep(1)
+    exit
+try:
+    amountOfUsers = int(input("Ange antal av användare att läsa in: "))
+except:
+    print("error")
+    print("Antal användare automatiskt angivet som 1")
+    amountOfUsers = 1
 lista = []
 listRow = 1
+command = "None"
 with open(str(filnamn), newline='') as myFile:
     reader = csv.reader(myFile)
     for row in reader:
         lista.append(row)
 
-def listAssign():
-    uName = str(lista[1][0])
-    uGivenName = str(lista[1][1])
-    uSurname = str(lista[1][2])
-    uAccountName = str(lista[1][3])
+def listAssign(listRow):
+    uName = str(lista[listRow][0])
+    uGivenName = str(lista[listRow][1])
+    uSurname = str(lista[listRow][2])
+    uAccountName = str(lista[listRow][3])
     return (uName,uGivenName,uSurname,uAccountName)
 
 def createPassword():
@@ -21,7 +33,7 @@ def createPassword():
     chars2 = "1234567890"
     chars3 = "!%&/()=#"
     uPassword = ""
-    for c in range (3):
+    for bomboclaat in range (3):
         uPassword += random.choice(chars)
         uPassword += random.choice(chars1)
         uPassword += random.choice(chars2)
@@ -30,9 +42,27 @@ def createPassword():
     uPassword = ''.join(random.sample(uPassword,len(uPassword)))
     return uPassword
 
+
+
 uPassword = createPassword()
-uName, uGivenName, uSurname, uAccountName = listAssign()
-listAssign()
-createPassword()
+uName, uGivenName, uSurname, uAccountName = listAssign(listRow)
+global cmd
 cmd = 'New-ADUser -name "' + uName + '" -GivenName "' + uGivenName + '" -Surname "' + uSurname + '" -SamAccountName "' + uAccountName + '" -AccountPassword "' + uPassword + '" -Enable $true'
-print(cmd)
+
+if platform.system() == "Windows":
+    command = cmd
+    print("Windows system detected...")
+    time.sleep(1.5)
+elif platform.system() == "Linux":
+    command = unix
+    print("Linux system detected...")
+    time.sleep(1.5)
+else:
+    print("ERR0R0R PICNIC: Köp ny dator")
+
+while listRow < amountOfUsers:
+    createPassword()
+    listAssign(listRow)
+    print(command)
+    listRow += 1
+    print(listRow)
